28a61f0517f87f1603ac6155064680ead387ec68
Hamzah Hasan <hamzahhasan123@gmail.com>
2025-10-19 11:40:37 +0700

    initial setup

---
+++ src/app/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="text-sm text-center italic">
+      <p>
+        &copy; CuyFace. Ramalan ini berdasarkan model AI vision, bukan untuk
+        dipercaya sepenuhnya.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
